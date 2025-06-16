99414971018c90de53ea5a4c00c716b3e307a859
Stephen Sherratt <stephen@sherra.tt>
2025-06-17 00:50:24 +1000

    Add macos build script for the compiler

---
+++ compiler/5.3.1/build-macos-aarch64.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+set -e
+
+TMP=$(mktemp -d)
+echo $TMP
+trap 'rm -rf $TMP' EXIT
+
+ORIGINAL_DIR="$PWD"
+
+cd "$TMP"
+wget https://s3.g.s4.mega.io/ycsnsngpe2elgjdd2uzbdpyj6s54q5itlvy6g/spice/compiler-sources/ocaml-relocatable-5.3.1.tar.gz
+tar xf ocaml-relocatable-5.3.1.tar.gz
+cd ocaml-relocatable-5.3.1
+./configure --prefix="$TMP/ocaml.5.3.1"
+make -j
+make install
+cd ..
+tar czf ocaml.5.3.1.tar.gz ocaml.5.3.1
+cp ocaml.5.3.1.tar.gz "$ORIGINAL_DIR/ocaml-macos.5.3.1.tar.gz"
