43fee3cda35dc320b5b215309dd2a9cefea4fc81
Stephen Sherratt <stephen@sherra.tt>
2025-07-26 22:12:08 +1000

    Complete renaming of tools

--- tool-build-scripts/5.3.1/ocamllsp.1.22.0/build-x86_64-linux-gnu.sh
+++ tool-build-scripts/5.3.1/ocamllsp.1.22.0/build-x86_64-linux-gnu.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -ex
 
-docker buildx build --output type=local,dest=./out -f ubuntu.dockerfile .
-mv out/ocamllsp-x86_64-linux-gnu.1.22.0-built-with-ocaml.5.3.1+relocatable.tar.gz ./ocamllsp-x86_64-linux-gnu.1.22.0-built-with-ocaml.5.3.1+relocatable.tar.gz
+docker buildx build --output type=local,dest=./out -f ubuntu-x86_64.dockerfile .
+mv out/ocamllsp-1.22.0-built-with-ocaml-5.3.1+relocatable-x86_64-linux-gnu.tar.gz .
 rm -rf out
--- tool-build-scripts/5.3.1/ocamllsp.1.22.0/build-x86_64-linux-musl-static.sh
+++ tool-build-scripts/5.3.1/ocamllsp.1.22.0/build-x86_64-linux-musl-static.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -ex
 
-docker buildx build --output type=local,dest=./out -f alpine.dockerfile .
-mv out/ocamllsp-x86_64-linux-musl-static.1.22.0-built-with-ocaml.5.3.1+relocatable.tar.gz ./ocamllsp-x86_64-linux-musl-static.1.22.0-built-with-ocaml.5.3.1+relocatable.tar.gz
+docker buildx build --output type=local,dest=./out -f alpine-x86_64.dockerfile .
+mv out/ocamllsp-1.22.0-built-with-ocaml-5.3.1+relocatable-x86_64-linux-musl-static.tar.gz .
 rm -rf out
