d1c32987c6b30f3a98fade160a126c577a79d4fc
Stephen Sherratt <stephen@sherra.tt>
2025-10-24 00:52:23 +1100

    Tweak CLI around dot files

--- alice/src/dot.ml
+++ alice/src/dot.ml
@@ -2,28 +2,29 @@ open! Alice_stdlib
 open Alice_project
 open Climate
 
-let dot =
+let dot_artifacts =
   let open Arg_parser in
   let+ () = Common.set_globals_from_flags
-  and+ project = Common.parse_project
-  and+ packages = flag [ "packages" ] ~doc:"Print package dependency graph."
-  and+ artifacts =
-    flag [ "artifacts" ] ~doc:"Print build artifact dependency graph (default)."
-  in
-  if packages && artifacts
-  then
-    Alice_error.user_exn [ Pp.text "--packages and --artifacts are mutually exclusive" ];
-  let dot_source =
-    if packages
-    then Project.dot_package_dependencies project
-    else Project.dot_build_artifacts project
-  in
-  print_endline dot_source
+  and+ project = Common.parse_project in
+  print_endline @@ Project.dot_build_artifacts project
+;;
+
+let dot_packages =
+  let open Arg_parser in
+  let+ () = Common.set_globals_from_flags
+  and+ project = Common.parse_project in
+  print_endline @@ Project.dot_package_dependencies project
 ;;
 
 let subcommand =
   let open Command in
   subcommand
     "dot"
-    (singleton ~doc:"Print graphviz source describing build artifact dependencies." dot)
+    (group
+       ~doc:"Print graphviz source files."
+       [ subcommand "artifacts" (singleton ~doc:"Vizualize the build plan." dot_artifacts)
+       ; subcommand
+           "packages"
+           (singleton ~doc:"Vizualize the package dependency graph." dot_packages)
+       ])
 ;;
