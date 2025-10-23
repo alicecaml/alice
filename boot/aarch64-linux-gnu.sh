69c8b609014fb58c7a52554ffda97746e098ddc5
Stephen Sherratt <stephen@sherra.tt>
2025-10-23 20:19:02 +1100

    Fix incorrect hashes in boot script

--- boot/aarch64-linux-gnu.sh
+++ boot/aarch64-linux-gnu.sh
@@ -20,8 +20,8 @@ wget "$BASE_URL/$OCAMLLSP.tar.gz"
 wget "$BASE_URL/$OCAMLFORMAT.tar.gz"
 
 echo c89f1fc2a34222a95984a05e823a032f5c5e7d6917444685d88e837b6744491a  $OCAML.tar.gz | sha256sum -c -
-echo 28bceaceeb6055fada11cf3ba1dcc3ffec4997925dee096a736fdaef4d370e56  $OCAMLLSP.tar.gz | sha256sum -c -
-echo 05ee153f176fbf077166fe637136fc679edd64a0942b8a74e8ac77878ac25d3f  $OCAMLFORMAT.tar.gz | sha256sum -c -
+echo 05ee153f176fbf077166fe637136fc679edd64a0942b8a74e8ac77878ac25d3f  $OCAMLLSP.tar.gz | sha256sum -c -
+echo 28bceaceeb6055fada11cf3ba1dcc3ffec4997925dee096a736fdaef4d370e56  $OCAMLFORMAT.tar.gz | sha256sum -c -
 
 tar xf "$OCAML.tar.gz"
 tar xf "$OCAMLLSP.tar.gz"
