3822d64e94ddb9ed4e0a6bf42a7f98c656d2c41b
Stephen Sherratt <stephen@sherra.tt>
2025-11-03 00:09:35 +1100

    Most file operations require absolute paths

--- io/src/file_ops.mli
+++ io/src/file_ops.mli
@@ -1,23 +1,35 @@
 open! Alice_stdlib
 open Alice_hierarchy
 
-val rm_rf : _ Path.t -> unit
-val mkdir_p : _ Path.t -> unit
+val rm_rf : Path.Absolute.t -> unit
+val mkdir_p : Path.Absolute.t -> unit
 
 (** [src] and [dst] must be paths to existing directories. Each file under
     [src] is moved (renamed) to the same relative path under [dst], creating
     intermediate directories as needed. The existing directory structure under
     [dst] is replaced, though files will be replaced if they are at the same
     location as a correspondingly-named file under [src]. *)
-val recursive_move_between_dirs : src:_ Path.t -> dst:_ Path.t -> unit
+val recursive_move_between_dirs : src:Path.Absolute.t -> dst:Path.Absolute.t -> unit
 
-val cp_rf : src:_ Path.t -> dst:_ Path.t -> unit
-val cp_f : src:_ Path.t -> dst:_ Path.t -> unit
+val cp_rf : src:Path.Absolute.t -> dst:Path.Absolute.t -> unit
+val cp_f : src:Path.Absolute.t -> dst:Path.Absolute.t -> unit
 val exists : _ Path.t -> bool
-val is_directory : _ Path.t -> bool
-val with_out_channel : _ Path.t -> mode:[ `Text | `Bin ] -> f:(out_channel -> 'a) -> 'a
-val write_text_file : _ Path.t -> string -> unit
-val with_in_channel : _ Path.t -> mode:[ `Text | `Bin ] -> f:(in_channel -> 'a) -> 'a
-val read_text_file : _ Path.t -> string
-val mtime : _ Path.t -> float
-val symlink : src:_ Path.t -> dst:_ Path.t -> unit
+val is_directory : Path.Absolute.t -> bool
+
+val with_out_channel
+  :  Path.Absolute.t
+  -> mode:[ `Text | `Bin ]
+  -> f:(out_channel -> 'a)
+  -> 'a
+
+val write_text_file : Path.Absolute.t -> string -> unit
+
+val with_in_channel
+  :  Path.Absolute.t
+  -> mode:[ `Text | `Bin ]
+  -> f:(in_channel -> 'a)
+  -> 'a
+
+val read_text_file : Path.Absolute.t -> string
+val mtime : Path.Absolute.t -> float
+val symlink : src:Path.Absolute.t -> dst:Path.Absolute.t -> unit
--- manifest/src/alice_manifest.mli
+++ manifest/src/alice_manifest.mli
@@ -2,6 +2,6 @@ open! Alice_stdlib
 open Alice_hierarchy
 
 val manifest_name : string
-val read_package_dir : dir_path:_ Path.t -> Package.t
-val read_package_manifest : manifest_path:_ Path.t -> Package.t
-val write_package_manifest : manifest_path:_ Path.t -> Package.t -> unit
+val read_package_dir : dir_path:Path.Absolute.t -> Package.t
+val read_package_manifest : manifest_path:Path.Absolute.t -> Package.t
+val write_package_manifest : manifest_path:Path.Absolute.t -> Package.t -> unit
