6777b5f4c284c97e42c6b7a0d057289e2bf2c781
Stephen Sherratt <stephen@sherra.tt>
2025-11-03 17:21:39 +1100

    Fix warning

--- stdlib/src/type_bool.ml
+++ stdlib/src/type_bool.ml
@@ -1,5 +1,14 @@
-type true_t = True_t
-type false_t = False_t
+(* Note that without disabling warning 37 the compiler warns about unused
+   constructors. While [true_t] and [false_t] would ideally be phantom types
+   (with no constructors), if the constructors are not exposed in the interface
+   then the compiler can't refute impossible patterns of GADTs involving these
+   types. An alternative solution would be to expose the constructors without
+   using the "private" keyword, however this would allow client code to
+   construct values of [true_t] and [false_t] which is pointless as they are
+   ideally phantom types. *)
+
+type true_t = True_t [@@warning "-37"]
+type false_t = False_t [@@warning "-37"]
 
 type _ t =
   | True : true_t t
