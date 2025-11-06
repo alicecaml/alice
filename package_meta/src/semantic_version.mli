62fab9310ba6ff1510fd6fa47481bb9efb6e866f
Stephen Sherratt <stephen@sherra.tt>
2025-11-07 10:32:28 +1100

    Minor doc addition

--- package_meta/src/semantic_version.mli
+++ package_meta/src/semantic_version.mli
@@ -9,8 +9,12 @@ val to_dyn : t -> Dyn.t
 val equal : t -> t -> bool
 val compare : t -> t -> int
 val to_string : t -> string
+
+(** The version number with a leading "v" *)
 val to_string_v : t -> string
+
+(** Returns an error if the argument isn't a valid semantic version. *)
 val of_string : string -> t user_result
 
-(** Raises a user error *)
+(** Raises a user error if the argument isn't a valid semantic version. *)
 val of_string_exn : string -> t
