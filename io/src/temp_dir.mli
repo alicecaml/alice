aad0d8efe9061f3e96e239f307e75b53f82b2cb1
Stephen Sherratt <stephen@sherra.tt>
2025-11-11 10:24:13 +1100

    Fix issue with max int on 32 bit machines

--- io/src/temp_dir.mli
+++ io/src/temp_dir.mli
@@ -1,10 +1,6 @@
 open! Alice_stdlib
 open Alice_hierarchy
 
-(** Make a new directory in the system's temporary directory returning its
-    path. Does not attempt to clean up after itself. *)
-val mkdir : prefix:string -> suffix:string -> Absolute_path.non_root_t
-
 (** Make a new directory in the system's temporary directory calling [f] on its
     path, returning the result of [f] and deleting the temporary directory
     after [f] returns. *)
