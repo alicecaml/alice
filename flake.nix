f7912d48ce593e2a7e5327f83ce0c86affcb0b12
Stephen Sherratt <stephen@sherra.tt>
2025-11-12 17:17:37 +1100

    Version bump

--- flake.nix
+++ flake.nix
@@ -46,7 +46,7 @@
         ];
         aliceMin = pkgs.ocamlPackages.buildDunePackage {
           pname = "alice";
-          version = "0.1.0";
+          version = "0.1.1-rc1";
           src = ./.;
           buildInputs = deps;
         };
