2f90ed4209dbbd5d0d29e9c34be0a9bb6b9c2ad7
·𐑑𐑴𐑕𐑑𐑩𐑤 <toastal@posteo.net>
2025-12-03 01:12:07 +0700

    nix: move climate to overlay

--- nix/overlay/default.nix
+++ nix/overlay/default.nix
@@ -2,4 +2,18 @@ final: prev: {
   alice = {
     package = final.callPackage ../package/alice.nix { };
   };
+
+  ocamlPackages = prev.ocamlPackages.overrideScope (
+    ofinal: oprev: {
+      climate = ofinal.buildDunePackage (finalAttrs: {
+        pname = "climate";
+        version = "0.9.0";
+        src = final.fetchgit {
+          url = "https://github.com/gridbugs/climate";
+          rev = "refs/tags/${finalAttrs.version}";
+          hash = "sha256-WRhWNWQ4iTUVpJlp7isJs3+0n/D0gYXTxRcCTJZ1o8U=";
+        };
+      });
+    }
+  );
 }
