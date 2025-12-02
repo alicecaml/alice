40318850d3e4f8bbeaa8188029442f228ef10076
Stephen Sherratt <stephen@sherra.tt>
2025-12-02 11:16:45 +1100

    Share shell.nix in flake

--- flake.nix
+++ flake.nix
@@ -12,19 +12,6 @@
       let pkgs = import nixpkgs { inherit system; };
       in {
         packages = pkgs.callPackage ./default.nix { };
-        devShell =
-          # When developing alice, use the musl toolchain. The development
-          # environment (ocamllsp, ocamlopt, etc) can then be managed by alice
-          # itself, since alice can install tools which have been pre-compiled
-          # against musl. This lets us dogfood alice's tool installation
-          # mechanism, and allows alice to be built with dune package
-          # management on NixOS, where the OCaml compiler is treated as a
-          # package and thus a specific version unknown to nix is fixed in the
-          # lockdir.
-          let muslPkgs = pkgs.pkgsMusl;
-          in muslPkgs.mkShell {
-            nativeBuildInputs = [ pkgs.graphviz ];
-            buildInputs = [ muslPkgs.musl ];
-          };
+        devShell = (import ./shell.nix { inherit pkgs; });
       });
 }
