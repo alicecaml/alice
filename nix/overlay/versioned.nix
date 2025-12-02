4cf83d18dac6a7abe0e25f5fc7fa5bf1cc552f39
·𐑑𐑴𐑕𐑑𐑩𐑤 <toastal@posteo.net>
2025-12-03 02:16:47 +0700

    nix: make versioned overlay

---
+++ nix/overlay/versioned.nix
@@ -0,0 +1,48 @@
+final: prev: {
+  alice =
+    let
+      versioned =
+        final.lib.mapAttrs
+          (
+            _:
+            { version, hash }:
+            final.alice.package.overrideAttrs (old: {
+              inherit version;
+              src = final.fetchgit {
+                inherit hash;
+                url = "https://github.com/alicecaml/alice";
+                rev = "refs/tags/${version}";
+              };
+            })
+          )
+          {
+            "0_1_0" = {
+              version = "0.1.0";
+              hash = "sha256-Ax9qbFzgHPH0EYQrgA+1bEAlFinc4egNKIn/ZrxV5K4=";
+            };
+            "0_1_1" = {
+              version = "0.1.1";
+              hash = "sha256-4T6YyyN4ttFcqSeBWNfff8bL7bYWYhLMxqRN7KCAp3c=";
+            };
+            "0_1_2" = {
+              version = "0.1.2";
+              hash = "sha256-05EXQxosue5XEwAUtkI/2VObKJzUTzrZfVH3WELHACk=";
+            };
+            "0_1_3" = {
+              version = "0.1.3";
+              hash = "sha256-PkZbzqjlWswJ/8wBJikj45royPUEyUWG/bRqB47qkXg=";
+            };
+            "0_2_0" = {
+              version = "0.2.0";
+              hash = "sha256-QNAPIccp3K6w0s35jmEWodwvac0YoWUZr0ffXptfLGs=";
+            };
+          };
+    in
+    (prev.alice or { })
+    // {
+      versioned = versioned // {
+        latest = versioned."0_2_0";
+        dev = final.alice.package;
+      };
+    };
+}
